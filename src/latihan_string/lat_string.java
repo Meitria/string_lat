14cf2e191c1f68431ad7fd713ef325178e721eff
LENOVO <LENOVO@DESKTOP-5G795P9>
2024-02-16 17:48:20 +0700

    first commit

---
+++ src/latihan_string/lat_string.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan_string;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class lat_string {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(lat_string[] args) {
+        String string="Belajar Pemrograman Java";
+        string.concat("Membuat Data bentuk string");
+        System.out.print(string);
+    }
+    
+}
